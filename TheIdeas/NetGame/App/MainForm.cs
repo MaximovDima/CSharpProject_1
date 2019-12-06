715d7574186d8b8d06170888b1566a0817ac9347
Maximov <pub_lic@mail.ru>
2019-12-07 00:07:33 +0700

    AddProject

---
+++ TheIdeas/NetGame/App/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NetGame
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
