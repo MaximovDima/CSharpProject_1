7e577f8c600e55ef8c3c05c2863620a2fb56083e
Maximov <pub_lic@mail.ru>
2020-02-29 21:10:04 +0700

    dir_model

--- TheIdeas/MW/Forms/EditFinance.cs
+++ TheIdeas/MW/Forms/EditFinance.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace MW.Forms
 {
 	public partial class FrmEditFinance : Form
 	{
+		//Списки значений для справочников
+		public List<string> TypesCost;
+		
 		public FrmEditFinance()
 		{
 			InitializeComponent();
+			SyncForm();
+		}
+		
+		//синхронизация формы(параметров, выпадающих списков)
+		public void SyncForm()
+		{
+			SyncValuesForComboBoxes();
+		}
+		
+		public void SyncValuesForComboBoxes()
+		{
+			SyncTypesCost();
+//			SyncPlaces();
+//			SyncTags();
+		}
+		
+		public void SyncTypesCost()
+		{
+			TypesCost = new List<string>();
+//			Data.FillListValues(TypesCost, Models.Dy
 		}
 		
 		void AddTypeCostClick(object sender, EventArgs e)
